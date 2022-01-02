129c7098e7c83825158971f2449b05979305c5b3
umitdogan33 <umit9551@gmail.com>
2022-01-02 12:00:04 +0300

    bug fixed

--- v1/src/controllers/Users.js
+++ v1/src/controllers/Users.js
@@ -20,11 +20,11 @@ const login = (req,res)=>{
     req.body.password = passwordToHash(req.body.password)
     loginUser(req.body).then((response)=>{
         if(!response) return res.status(httpStatus.NOT_FOUND).send({message:"Böyle Bir Kullanıcı Bulunamadı"})
-        console.log("login_response",response)
+
         response = {
             ...response.toObject(),
             tokens:{
-                access_token : generateAccessToken(response ),
+                access_token : generateAccessToken(response),
                 refresh_token : generateRefreshToken(response)
             },
         };
@@ -57,20 +57,17 @@ const projectList = (req,res)=>{
 
 const resetPassword = (req,res)=> {
     const new_password = uuid.v4()?.split("-")[0] || new Date().getTime();
-
     modify({email: req.body.email}, {password: passwordToHash(new_password)}).then((response) => {
         if (!response) {
             return res.status(httpStatus.NOT_FOUND).send({error: "böyle bir kullanıcı bulunmamaktadır"})
         }
-        console.log("satır 65",req.body.email)
-        eventEmitter.emit("send_email", {
-            // from: response.email
 
-            to: res.email,
+        eventEmitter.emit("send_email", {
+            to: req.body.email,
             subject: "Şifre Sıfırlama", // Subject line
             html: `Şifre Sıfırlandı<br/>Yeni Şifre<br/><b>Şifre:${new_password}</b>`, // html body
         })
-        console.log(response.email)
+
         res.status(httpStatus.OK).send("isteğiniz üzerine sıfırlama işlemi oluşmuştur epastaya bak");
 
     }).catch((e)=>{
--- v1/src/scripts/events/index.js
+++ v1/src/scripts/events/index.js
@@ -4,8 +4,12 @@ const nodemailer = require("nodemailer");
 module.exports = () =>{
 eventEmitter.on("send_email",(emailData)=>{
     let transporter = nodemailer.createTransport({
+        // host: process.env.EMAIL_HOST,
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+        secure: false,
+        service:"gmail",
+        requireTLS: true,
         auth: {
             user: process.env.EMAIL_USER, // generated ethereal user
             pass: process.env.EMAIL_PASSWORD, // generated ethereal password
